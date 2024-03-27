cc28c1992b7c8890a1d9d867f7945755485c32eb
Adolfo <adolfochaveziv@gmail.com>
2024-03-27 10:58:32 -0500

    create README.md and modify main

---
+++ README.md
@@ -0,0 +1,12 @@
+# Operating Systems Project 2
+
+Hi, this is my second OpSys project. I'm kind of rushing it.
+The function is to scan an input file for 4 things: Dimensions m and n of a matrix, an m x n matrix, another m x n matrix, and a vector of size m.
+
+From this, I'm supposed to determine if any processes are deadlocked? I think it's doable. So uh yeah let's get started
+
+## The process
+
+I need to define my data structures first. I think I can do multilayered arrays in C++ to make my matrices and vectors, so let's go with that
+
+Ok first I need to relearn how to read the content of a file
--- kitty_cat_coding.cpp
+++ kitty_cat_coding.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 // Step 1: Data Structures. There are 3: Available, Allocation, and Request.
-// Available: This is a VECTOR of length m that indicates the number of resources available for a specific resource type.
+// Available: This is a VECTOR of size m (a row of the matrix) that indicates the number of resources available for a specific resource type.
 // Allocation: This is an m x n matrix that indicates the number of resources of each type that are CURRENTLY allocated to each process.
 // Request: This is another m x n matrix that indicates the current REQUEST of each process.
-// Work: A VECTOR of size m.
-// Finish: A VECTOR of size n. (Distinct from Work!)
+// Work: A VECTOR of size m. (A row of the matrix)
+// Finish: A VECTOR of size n. (Distinct from Work! This is a COLUMN of the matrix)
 
 // I should probably also make some global variables, like m and n, which are the dimensions of the input matrix.
 // I will keep the content of the file being scanned as a variable in main. Doesn't need to be global, because I can just scan the relevant info to other variables.
@@ -28,7 +28,23 @@ int main(string fileName)
         cout << "Please pass your input file name as an argument to this program.\n";
         cout << "See ya!\n";
     } else {
-        string content;
+        // I forgor how to read content from a file :skull:
+
+        // STEPS to reading the content of a file:
+        // 1. Create a stream object.
+        // 2. Connect it to a file on disk. (probably the fstream function)
+        // 3. Read the file's contents into our stream object. (We need a string function declared beforehand for this)
+        // 4. Close the file. 
+
+        string contentText;
+
+        // The object in question:
+        ifstream file(fileName);
+
+        while (getline(file, contentText))
+        {
+            cout << contentText;
+        }
     }
 
     return 0;
