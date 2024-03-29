f68c79f44f1f48376e2a3f58a20a9facc2482a06
Adolfo <adolfochaveziv@gmail.com>
2024-03-29 15:35:55 -0500

    added functional file scan and variable assignment in main

--- kitty_cat_coding.cpp
+++ kitty_cat_coding.cpp
@@ -12,9 +12,10 @@ using namespace std;
 // I should probably also make some global variables, like m and n, which are the dimensions of the input matrix.
 // I will keep the content of the file being scanned as a variable in main. Doesn't need to be global, because I can just scan the relevant info to other variables.
 
-// m is the number of rows.
-// n is the number of columns.
-int m, n;
+// m = number of rows.
+int m;
+// n = number of columns.
+int n;
 
 // main has an argument you need to pass it. This argument is a string indicating the address of the
 // file you want to scan.
@@ -34,15 +35,15 @@ int main(int argc, char* argv[])
         cout << "m = number of matrix rows\n";
         cout << "n = number of matrix columns\n\n";
 
-        cout << "   ,-.\n _(*_*)_\n(_  o  _)\n  / o \\\n (_/ \\_)\n\n"; // Art
+        cout << "      \\    /\\\n       )  ( ')\n      (  /  )\n       \\(__)|\n\n"; // Art
 
         cout << "See ya!\n";
     } else {
+// EVERYTHING BELOW HERE CONTAINS MAIN FUNCTION OPERATIONS AND INITIALIZERS
 
         string fileName = argv[1];
 
         // I forgor how to read content from a file :skull:
-
         // STEPS to reading the content of a file:
         // 1. Create a stream object.
         // 2. Connect it to a file on disk. (probably the fstream function)
@@ -65,18 +66,62 @@ int main(int argc, char* argv[])
             // was returning 51 earlier. But anyway, that's what makes it work.
             // That's how the char data type works. The character 'A' has an ASCII value of 65.
             // So if you wanted to, you could declare this character by saying char lettera = 64 + 1;.
+            cout << contentText << "\n";
             if (lineCount == 0)
             {
                 m = contentText[0] - '0';
                 n = contentText[2] - '0';
             }
+
             lineCount++;
         }
+
         // REMBER:
         // m is the number of rows.
         // n is the number of columns.
         cout << "m equals: " << m << "\n";
         cout << "n equals: " << n << "\n";
+
+        // Erm it didn't let me declare this because I put it right next to int m, n; which makes
+        // the value of m and n garbage data. 
+        // Variables with indeterminate values are variables you define like how you did earlier
+        // with the int m, n; line. You should study these, they're straightforward but cool. You
+        // generally can't access other programs' data with these though, they literally just contain
+        // random values, which goes back to the fact that this entire program is in its own block of memory
+
+        // The m x n matrix that indicates the number of resources of each type currently allocated to each process.
+        int allocation[m][n];
+
+        // I have to run this function again because the last one gave me the variables I need to be able
+        // to use these variables.
+
+        ifstream file2(fileName);
+
+        // ASSIGN MATRIX VALUES
+        lineCount = 0;
+        while (getline(file2, contentText))
+        {
+            if (lineCount != 0)
+            {
+                // ASSIGN MATRIX VALUES
+                for(int i = 0; i < n; i++)
+                {
+                    allocation[lineCount - 1][i] = contentText[0 + (2 * i)] - '0';
+                }
+            }
+            cout << "+ Allocation matrix values assigned\n";
+            lineCount++;
+        }
+        // Print an entire matrix
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << allocation[i][j] << " ";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
     }
 
     return 0;
